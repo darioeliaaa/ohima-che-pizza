65e68e72ab73fc0d9b376882abca1a0cf9c45821
darioeliaaa <darioelia61@gmail.com>
2026-07-06 19:51:17 +0200

    u

---
+++ frontend/src/app/pages/privacy/privacy.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  templateUrl: './privacy.html', // SOLO QUESTO, niente /src/app/...
+  styleUrls: ['./privacy.css']
+})
+export class privacy { }
